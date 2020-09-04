15d322fc6664ce0099b2e1e9ce5ec3e0fa915416
ykrop22 <ilya.gaidai2001@gmail.com>
2020-09-04 18:01:11 +0300

    second3124

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Hello world2!");
+            Console.WriteLine("Hello world67891242!");
         }
     }
 }
