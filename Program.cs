b10c03657ac3dfb33d8886c6dcf6aba8c9ae6f86
ykrop22 <ilya.gaidai2001@gmail.com>
2020-09-04 17:58:55 +0300

    first cxommit22

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Hello world22!");
         }
     }
 }
